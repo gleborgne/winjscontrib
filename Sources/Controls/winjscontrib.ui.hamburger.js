d56c0cc09b7153c92461d162fcd9480ff30511ed
Mehdi Lahlou <lahlou.mehdi@gmail.com>
2015-05-26 12:12:56 +0200

    winjscontrib.ui.hamburger

---
+++ Sources/Controls/winjscontrib.ui.hamburger.js
@@ -0,0 +1,41 @@
+﻿(function () {
+    'use strict';
+    WinJS.Namespace.define("WinJSContrib.UI", {
+        Hamburger: WinJS.Class.define(
+            function ElasticInput(element, options) {
+                var ctrl = this;
+                options = options || {};
+                ctrl.element = element || document.createElement('div');
+                ctrl.element.classList.add('menu-button');
+                ctrl.element.innerHTML = '<div class="menu-button-wrapper"><span class="menu-global menu-top"></span><span class="menu-global menu-middle"></span><span class="menu-global menu-bottom"></span></div>';
+            }, {
+                activate: function () {
+                    ctrl.element.classList.add('active');
+                },
+                deactivate: function () {
+                    ctrl.element.classList.remove('active');
+                },
+                on: function () {
+                    var ctrl = this;
+                    var list = ctrl.element.querySelectorAll('.menu-global');
+                    for (var i = 0; i < list.length; i++) {
+                        list[i].classList.add('activated');
+                    }
+                },
+                off: function () {
+                    var ctrl = this;
+                    var list = ctrl.element.querySelectorAll('.menu-global');
+                    for (var i = 0; i < list.length; i++) {
+                        list[i].classList.remove('activated');
+                    }
+                },
+                tap: function (callBack) {
+                    if (callBack)
+                        WinJSContrib.UI.tap(this.element, callBack);
+                    else {
+                        WinJSContrib.UI.untap(this.element);
+                    }
+                }
+            })
+    });
+})();
