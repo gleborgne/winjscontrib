f08b08ad6c920d22e3d462b7963de964cfab8b56
gleborgne <gleborgne@mcnext.com>
2015-09-15 10:42:18 +0200

    dist

---
+++ dist/bin/ts/winjscontrib.datacontainer.localstorage.d.ts
@@ -0,0 +1,22 @@
+﻿declare var __global: any;
+declare module WinJSContrib.DataContainer {
+    var current: IDataContainer;
+    class LocalStorageContainer implements IDataContainer {
+        key: string;
+        parent: LocalStorageContainer;
+        useDataCache: boolean;
+        storageKey: string;
+        options: any;
+        childs: any;
+        static makeCurrent(key?: string, options?: any): void;
+        constructor(key?: string, options?: any, parent?: LocalStorageContainer);
+        read(itemkey: any): WinJS.IPromise<any>;
+        save(itemkey: any, obj: any): WinJS.IPromise<{}>;
+        remove(itemkey: any): WinJS.IPromise<{}>;
+        listKeys(): WinJS.IPromise<any[]>;
+        list(): WinJS.IPromise<any[]>;
+        child(key: any): LocalStorageContainer;
+        childWithTransaction(key: string, process: (arg: LocalStorageContainer) => WinJS.Promise<any>): WinJS.Promise<LocalStorageContainer>;
+        deleteContainer(): WinJS.Promise<any>;
+    }
+}
