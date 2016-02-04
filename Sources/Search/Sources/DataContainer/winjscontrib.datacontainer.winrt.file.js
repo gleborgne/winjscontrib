f67ee30fd9b1153f0e790736af079ffd7a47deea
Strato <francois-guillot@wanadoo.fr>
2016-02-04 18:05:22 +0100

    Fix for datacontainer.listkeys
    To take only files that ENDS with '.json', not just contains it (like '.json.TMP' for example).

--- Sources/Search/Sources/DataContainer/winjscontrib.datacontainer.winrt.file.js
+++ Sources/Search/Sources/DataContainer/winjscontrib.datacontainer.winrt.file.js
@@ -72,15 +72,19 @@ var WinJSContrib;
             WinRTFilesContainer.prototype.listKeys = function () {
                 var container = this;
                 return container.folderPromise.then(function (folder) {
-                    return folder.getFilesAsync().then(function (files) {
-                        return files.map(function (f) {
-                            var key = f.path.substr(folder.path.length + 1);
-                            var ext = key.indexOf('.json');
-                            if (ext > 0) {
-                                key = key.substr(0, ext);
+                    var queryOptions = new Windows.Storage.Search.QueryOptions(Windows.Storage.Search.CommonFileQuery.defaultQuery, [".json"]);
+                    return folder.createFileQueryWithOptions(queryOptions).getFilesAsync().then(function (files) {
+                        return files.reduce(function (seed, file) {
+                            var fileName = file.path.substr(folder.path.length + 1);
+                            var indexOfExtension = fileName.indexOf('.json');
+
+                            if (indexOfExtension > 0) {
+                                var fileNameWithoutExtension = fileName.substr(0, indexOfExtension);
+                                seed.push(fileNameWithoutExtension);
                             }
-                            return key;
-                        });
+
+                            return seed;
+                        }, []);
                     });
                 });
             };
